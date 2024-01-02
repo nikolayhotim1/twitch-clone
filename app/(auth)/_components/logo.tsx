5b5a064fb6df1a37c623546c7d2c39537e3eb8fb
nikolayhotim1 <hotim_nikolay@mail.ru>
2024-01-02 03:09:31 +0300

    feature: dark mode

---
+++ app/(auth)/_components/logo.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image'
+import { Poppins } from 'next/font/google'
+import { cn } from '@/lib/utils'
+
+const font = Poppins({
+	subsets: ['latin'],
+	weight: ['200', '300', '400', '500', '600', '700', '800']
+})
+
+export function Logo() {
+	return (
+		<div className='flex flex-col items-center gap-y-4'>
+			<div className='bg-white rounded-full p-1'>
+				<Image
+					src='/spooky.svg'
+					alt='Gamehub'
+					height='80'
+					width='80'
+				/>
+			</div>
+			<div
+				className={cn(
+					'flex flex-col items-center',
+					font.className
+				)}
+			>
+				<p className='text-xl font-semibold'>Gamehub</p>
+				<p className='text-sm text-muted-foreground'>
+					Let&apos;s play
+				</p>
+			</div>
+		</div>
+	)
+}
