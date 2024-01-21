a11e2486f5dd82b4beb0e11df331d1ab91eaa76c
nikolayhotim1 <hotim_nikolay@mail.ru>
2024-01-22 00:23:27 +0300

    feature: home page

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,10 +1,12 @@
-import { UserButton } from '@clerk/nextjs'
+import { Suspense } from 'react'
+import { Results, ResultsSkeleton } from './_components/results'
 
 export default function Page() {
 	return (
-		<div className='flex flex-col gap-y-4'>
-			<h1>Home Page</h1>
-			<UserButton afterSignOutUrl='/' />
+		<div className='h-full p-8 max-w-screen-2xl mx-auto'>
+			<Suspense fallback={<ResultsSkeleton />}>
+				<Results />
+			</Suspense>
 		</div>
 	)
 }
