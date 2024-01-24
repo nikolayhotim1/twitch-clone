cdd85b2f76a7c35cb0b500ee907b7693a1e601b5
nikolayhotim1 <hotim_nikolay@mail.ru>
2024-01-24 23:44:52 +0300

    feature: unblock page

---
+++ app/(dashboard)/u/[username]/community/_components/unblock-button.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import { toast } from 'sonner'
+import { useTransition } from 'react'
+import { onUnblock } from '@/actions/block'
+import { Button } from '@/components/ui/button'
+
+interface UnblockButtonProps {
+	userId: string
+}
+
+export function UnblockButton({ userId }: UnblockButtonProps) {
+	const [isPending, startTransition] = useTransition()
+	function onClick() {
+		startTransition(() => {
+			onUnblock(userId)
+				.then(result =>
+					toast.success(`User ${result.blocked.username} unblocked`)
+				)
+				.catch(() => toast.error('Something went wrong'))
+		})
+	}
+	return (
+		<Button
+			disabled={isPending}
+			onClick={onClick}
+			variant='link'
+			size='sm'
+			className='text-blue-500 w-full'
+		>
+			Unblock
+		</Button>
+	)
+}
